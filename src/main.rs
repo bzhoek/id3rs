5881bb5c093b4540a4f05af10f473b3457858ec2
Bas van der Hoek <bas@hoek.com>
2021-07-05 17:20:18 +0200

    stop processing once we reach 00 byte frames

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,7 @@ use std::io::SeekFrom::Current;
 
 pub type Result<T> = std::result::Result<T, Box<dyn std::error::Error + Send + Sync>>;
 
+// https://id3.org/id3v2.3.0
 // https://mutagen-specs.readthedocs.io/en/latest/id3/id3v2.4.0-structure.html
 
 fn main() -> Result<()> {
@@ -11,14 +12,41 @@ fn main() -> Result<()> {
   let mut buffer = [0; 10];
   file.read_exact(&mut buffer)?;
   let signature = String::from_utf8(buffer[0..3].to_owned()).unwrap();
-  let size = u32::from_be_bytes(buffer[4..8].try_into().unwrap());
-  println!("sig {} {:?}", signature, buffer);
-  while true {
+  let max = syncsafe(&buffer[6..10]);
+  let mut pos = 0u64;
+  println!("sig {} size {} {:?}", signature, max, buffer);
+  loop {
     file.read_exact(&mut buffer)?;
-    let id = String::from_utf8(buffer[0..4].to_owned()).unwrap();
-    let size = u32::from_be_bytes(buffer[4..8].try_into().unwrap());
-    println!("frame {} size {} {:?}", id, size, buffer);
-    file.seek(Current(size as i64)).unwrap();
+    let idval = u32::from_be_bytes(buffer[0..4].try_into().unwrap());
+    if idval == 0 { break; }
+    let idstr = String::from_utf8(buffer[0..4].to_owned()).unwrap();
+    let size = syncsafe(&buffer[4..8]);
+    pos = file.seek(Current(size as i64)).unwrap();
+    println!("frame {} size {} pos {} {:?}", idstr, size, pos, buffer);
   }
   Ok(())
+}
+
+// only 7 bytes of each byte are significant
+fn syncsafe(bytes: &[u8]) -> u64 {
+  bytes.iter().fold(0, |result, byte| { result << 7 | (*byte as u64) })
+}
+
+#[cfg(test)]
+mod tests {
+  use crate::syncsafe;
+
+  #[test]
+  fn test_frame() {
+    let bytes = [0u8, 0, 0x73, 0x71];
+    let result = syncsafe(&bytes);
+    assert_eq!(14833, result);
+  }
+
+  #[test]
+  fn test_header() {
+    let bytes = [0u8, 0x02, 0x3e, 0x77];
+    let result = syncsafe(&bytes);
+    assert_eq!(40823, result);
+  }
 }

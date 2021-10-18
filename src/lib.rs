d5edc2d50fbb535a8e7da9ab7c9ab2e533d347be
Bas van der Hoek <bas@hoek.com>
2021-10-18 13:26:20 +0200

    parse the frame

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,11 @@
-use nom::bytes::streaming::tag;
+use std::str::from_utf8;
+
+use nom::bytes::streaming::{tag, take};
+use nom::combinator::map;
 use nom::IResult;
 use nom::multi::fold_many_m_n;
-use nom::number::streaming::be_u8;
+use nom::number::complete::be_u32;
+use nom::number::streaming::{be_u16, be_u8};
 use nom::sequence::tuple;
 
 #[derive(Debug, PartialEq, Eq)]
@@ -10,6 +14,13 @@ pub struct Header {
   revision: u8,
   flags: u8,
   next: u64,
+}
+
+#[derive(Debug, PartialEq, Eq)]
+pub struct Frame<'a> {
+  id: &'a str,
+  size: u32,
+  flags: u16,
 }
 
 fn file_header(input: &[u8]) -> IResult<&[u8], Header> {
@@ -21,6 +32,22 @@ fn file_header(input: &[u8]) -> IResult<&[u8], Header> {
       fold_many_m_n(4, 4, be_u8, 0u64, |acc, byte| acc << 7 | (byte as u64))
     ))(input)?;
   Ok((input, Header { version, revision, flags, next }))
+}
+
+fn id_as_str(input: &[u8]) -> IResult<&[u8], &str> {
+  map(
+    take(4u8),
+    |res| from_utf8(res).unwrap(),
+  )(input)
+}
+
+fn frame(input: &[u8]) -> IResult<&[u8], Frame> {
+  let (input, (id, size, flags)) = tuple((
+    id_as_str,
+    be_u32,
+    be_u16
+  ))(input)?;
+  Ok((input, Frame { id, size, flags }))
 }
 
 #[cfg(test)]
@@ -39,7 +66,7 @@ mod tests {
     let (_, header) = file_header(&buffer).ok().unwrap();
     assert_eq!(header, Header { version: 3, revision: 0, flags: 0, next: 46029 });
     file.read_exact(&mut buffer).unwrap();
-    // let (_, header) = file_header(&buffer).ok().unwrap();
-    // assert_eq!(header, Header { revision: 3, flags: 0, next: 359 });
+    let (_, frame) = frame(&buffer).ok().unwrap();
+    assert_eq!(frame, Frame { id: "TALB", size:39, flags:0 });
   }
 }

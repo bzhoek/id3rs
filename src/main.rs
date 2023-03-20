dab80a84d9b8ed7aeb3fd35f342139ece9dac85c
Bas van der Hoek <bas@hoek.com>
2023-03-20 14:43:37 +0100

    bump clap to 3

--- src/main.rs
+++ src/main.rs
@@ -25,14 +25,14 @@ fn main() -> Result<()> {
   Ok(())
 }
 
-pub fn debug_arg() -> Arg<'static, 'static> {
+pub fn debug_arg() -> Arg<'static> {
   Arg::with_name("DEBUG")
     .help("Show debug logging")
-    .short("d")
+    .short('d')
     .long("debug")
 }
 
-pub fn configure_logging(args: &ArgMatches<'_>) {
+pub fn configure_logging(args: &ArgMatches) {
   let filter = if args.is_present("DEBUG") { "debug,html5ever=info" } else { "info" };
   env_logger::Builder::from_env(Env::default().default_filter_or(filter)).target(Stdout).init();
   debug!("Debug logging");

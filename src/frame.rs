adc3131918b5bf00ba359396c2ad06d8b894cd80
Bas van der Hoek <bas@hoek.com>
2024-10-31 16:04:47 +0100

    Test frame size with simplified parsing

--- src/frame.rs
+++ src/frame.rs
@@ -41,21 +41,41 @@ impl From<u8> for Layer {
   }
 }
 
-fn bitrate_to_kbps(bitrate: u8) -> u32 {
-  match bitrate {
-    0b0001 => 32,
-    0b0010 => 40,
-    0b0011 => 56,
-    0b0100 => 64,
-    0b0101 => 80,
-    0b0110 => 96,
-    0b0111 => 112,
-    0b1000 => 128,
-    0b1001 => 160,
-    0b1010 => 192,
-    0b1011 => 224,
-    0b1100 => 256,
-    0b1110 => 320,
+fn bitrate_to_kbps(version: &Version, layer: &Layer, bitrate: u8) -> u32 {
+  match (version, layer) {
+    (Version::Version1, Layer::Layer3) => {
+      match bitrate {
+        0b0001 => 32,
+        0b0010 => 40,
+        0b0011 => 48,
+        0b0100 => 56,
+        0b0101 => 64,
+        0b0110 => 80,
+        0b0111 => 96,
+        0b1000 => 112,
+        0b1001 => 128,
+        0b1010 => 160,
+        0b1011 => 192,
+        0b1100 => 224,
+        0b1101 => 256,
+        0b1110 => 320,
+        _ => 0,
+      }
+    }
+    (_, _) => 0
+  }
+}
+
+fn sampling_to_hz(version: &Version, sampling: u8) -> u32 {
+  match version {
+    Version::Version1 => {
+      match sampling {
+        0b0000 => 44100,
+        0b0001 => 48000,
+        0b0010 => 32000,
+        _ => 0,
+      }
+    }
     _ => 0,
   }
 }
@@ -81,6 +101,41 @@ pub struct FrameHeader {
   pub layer: Layer,
   pub crc: Protection,
   pub bitrate: u32,
+  pub frequency: u32,
+}
+
+impl FrameHeader {
+  pub fn frame_size(&self) -> u32 {
+    144000 * self.bitrate / self.frequency
+  }
+}
+
+// http://id3lib.sourceforge.net/id3/mp3frame.html and http://www.mp3-tech.org/programmer/frame_header.html
+#[allow(dead_code, unused)]
+pub fn frame_header(input: &[u8]) -> IResult<&[u8], FrameHeader> {
+  let (input, _) = take_until(b"\xff".as_bytes())(input)?;
+  let (input, _) = nom::bytes::streaming::take(1u32)(input)?;
+  let (input, (version_u8, layer_u8, crc)) = bits(frame_header_layer)(input)?;
+  let (input, (bitrate_u8, sampling_u8, padding, private)) = bits(frame_header_bitrate)(input)?;
+  let (input, (channel, mode, copyright, original, emphasis)) = bits(frame_header_mode)(input)?;
+  println!("{:?}", input.len());
+
+  // let (input, frame) = nom::bits::complete::tag(0b111, 3usize)(input)?;
+  // let (_input, (bitrate, frequency, padding,private))
+  //   = bits(tuple((take(4usize), take(2usize), take(1usize), take(1usize))))(input)?;
+
+  let version = Version::from(version_u8);
+  let layer = Layer::from(layer_u8);
+  let bitrate = bitrate_to_kbps(&version, &layer, bitrate_u8);
+  let frequency = sampling_to_hz(&version, sampling_u8);
+  let frame = FrameHeader {
+    version,
+    layer,
+    crc: Protection::from(crc),
+    bitrate,
+    frequency,
+  };
+  Ok((input, frame))
 }
 
 fn frame_header_layer(i: (&[u8], usize)) -> IResult<(&[u8], usize), (u8, u8, u8)> {
@@ -110,27 +165,4 @@ fn frame_header_mode(i: (&[u8], usize)) -> IResult<(&[u8], usize), ModeFlags> {
   let (i, original) = take(1usize)(i)?;
   let (i, emphasis) = take(2usize)(i)?;
   Ok((i, (channel, mode, copyright, original, emphasis)))
-}
-
-// http://id3lib.sourceforge.net/id3/mp3frame.html and http://www.mp3-tech.org/programmer/frame_header.html
-#[allow(dead_code, unused)]
-pub fn frame_header(input: &[u8]) -> IResult<&[u8], FrameHeader> {
-  let (input, _) = take_until(b"\xff".as_bytes())(input)?;
-  let (input, _) = nom::bytes::streaming::take(1u32)(input)?;
-  let (input, (version, layer, crc)) = bits(frame_header_layer)(input)?;
-  let (input, (bitrate, frequency, padding, private)) = bits(frame_header_bitrate)(input)?;
-  let (input, (channel, mode, copyright, original, emphasis)) = bits(frame_header_mode)(input)?;
-  println!("{:?}", input.len());
-
-  // let (input, frame) = nom::bits::complete::tag(0b111, 3usize)(input)?;
-  // let (_input, (bitrate, frequency, padding,private))
-  //   = bits(tuple((take(4usize), take(2usize), take(1usize), take(1usize))))(input)?;
-
-  let frame = FrameHeader {
-    version: Version::from(version),
-    layer: Layer::from(layer),
-    crc: Protection::from(crc),
-    bitrate: bitrate_to_kbps(bitrate),
-  };
-  Ok((input, frame))
 }
--- tests/frame_test.rs
+++ tests/frame_test.rs
@@ -1,6 +1,68 @@
+pub struct Mp3Header<'a> {
+  pub bytes: &'a [u8; 4],
+}
+
+impl Mp3Header<'_> {
+  pub fn new(bytes: &[u8; 4]) -> Mp3Header {
+    Mp3Header { bytes }
+  }
+
+  pub fn version(&self) -> u8 {
+    (self.bytes[1] & 0b00011000) >> 3
+  }
+
+  pub fn layer(&self) -> u8 {
+    (self.bytes[1] & 0b00000110) >> 1
+  }
+
+  pub fn bitrate(&self) -> u8 {
+    (self.bytes[2] & 0b11110000) >> 4
+  }
+
+  pub fn frequency(&self) -> u8 {
+    (self.bytes[2] & 0b00001100) >> 2
+  }
+
+  pub fn kbit(&self) -> u32 {
+    match (self.version(), self.layer()) {
+      (0b11, 0b01) => {
+        match self.bitrate() {
+          0b0001 => 32,
+          0b0010 => 40,
+          0b0011 => 48,
+          0b0100 => 56,
+          0b0101 => 64,
+          0b0110 => 80,
+          0b0111 => 96,
+          0b1000 => 112,
+          0b1001 => 128,
+          0b1010 => 160,
+          0b1011 => 192,
+          0b1100 => 224,
+          0b1101 => 256,
+          0b1110 => 320,
+          _ => unreachable!("Invalid bitrate"),
+        }
+      }
+      _ => unreachable!("Invalid version or layer"),
+    }
+  }
+
+  pub fn len(&self) -> u32 {
+    let hz = match self.frequency() {
+      0b00 => 44100,
+      0b01 => 48000,
+      0b10 => 32000,
+      _ => unreachable!("Invalid frequency"),
+    };
+    144000 * self.kbit() / hz
+  }
+}
+
 #[cfg(test)]
 mod tests {
   use id3rs::frame::{frame_header, FrameHeader, Layer, Protection, Version};
+  use crate::Mp3Header;
 
   #[test]
   fn find_frame_header() {
@@ -13,6 +75,31 @@ mod tests {
       layer: Layer::Layer3,
       crc: Protection::Unprotected,
       bitrate: 160,
+      frequency: 44100,
     });
+  }
+
+  #[test]
+  fn parse_mp3_header() {
+    let bytes = b"\xFF\xFB\x94\x44";
+
+    let header = Mp3Header::new(bytes);
+    assert_eq!(header.len(), 384)
+  }
+
+  #[test]
+  fn parse_frame_header() {
+    let bytes = b"\xFF\xFB\x94\x44";
+
+    let (position, frame) = frame_header(bytes).ok().unwrap();
+    println!("{:?}", frame);
+    assert_eq!(frame, FrameHeader {
+      version: Version::Version1,
+      layer: Layer::Layer3,
+      crc: Protection::Unprotected,
+      bitrate: 128,
+      frequency: 48000,
+    });
+    assert_eq!(frame.frame_size(), 384)
   }
 }
